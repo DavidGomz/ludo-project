eabba987491f8d461027974f3c09126fd8ec35f0
David <david.henri.santana@gmail.com>
2022-09-08 08:43:08 -0300

    Sound
    sound img

--- src/pages/Game/index.tsx
+++ src/pages/Game/index.tsx
@@ -3,10 +3,14 @@ import superLudo from '../../assets/images/Super Ludo.svg';
 
 import entrar from '../../assets/images/Entrar.svg';
 
+import bgImage from '../../assets/images/bg-image.jpg';
+
+import volumeMedium from '../../assets/images/volume-medium.svg';
+
 export const Game = () => {
   return (
-    <div className="bg-slate-900  grid place-content-center h-screen ">
-      <div className="max-w-xl w-full h-96 pt-12 pb-16 pl-16 pr-16 bg-[#282A42] shadow-[#106ae0] shadow-lg ">
+    <div className=" bg-slate-900 min-h-full  w-full grid place-content-center h-screen ">
+      <div className="max-w-xl min-h-full min-w w-full h-96 pt-12 pb-16 pl-16 pr-16 bg-[#282A42] shadow-[#106ae0] shadow-lg ">
         <img src={superLudo} alt="game-Logo" />
         <form action="" className="flex flex-col justify-center  items-center">
           <input
@@ -19,6 +23,11 @@ export const Game = () => {
           </button>
         </form>
       </div>
+      <img
+        className="justify-self-end self-end mt-12 cursor-pointer"
+        src={volumeMedium}
+        alt="sound-page"
+      />
     </div>
   );
 };
